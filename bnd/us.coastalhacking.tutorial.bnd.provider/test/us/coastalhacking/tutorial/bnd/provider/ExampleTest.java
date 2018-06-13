10789c4819346c2ba0111dce4b7424970d25a1d9
Jon Passki <jon@passki.us>
2018-06-13 10:14:11 -0700

    Initial commit
    * New bnd workspace
    * New bnd project
      * Modified its test case to not fail
    * chmod +x gradlew
    
    Runs: `cd bnd; ./gradlew jar`

---
+++ bnd/us.coastalhacking.tutorial.bnd.provider/test/us/coastalhacking/tutorial/bnd/provider/ExampleTest.java
@@ -0,0 +1,14 @@
+package us.coastalhacking.tutorial.bnd.provider;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
